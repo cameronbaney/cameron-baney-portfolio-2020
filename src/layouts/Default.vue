ed706a37e6491a612dde8aac0da5bbc11a16a2c9
Cameron Baney <cbaney@gmail.com>
2020-03-04 19:35:01 -0500

    Initial Commit

---
+++ src/layouts/Default.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <header class="header">
+      <strong>
+        <g-link to="/">{{ $static.metadata.siteName }}</g-link>
+      </strong>
+      <nav class="nav">
+        <g-link class="nav__link" to="/">
+          Home
+        </g-link>
+        <g-link class="nav__link" to="/about/">
+          About
+        </g-link>
+      </nav>
+    </header>
+    <div class="container mx-auto px-4">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<static-query>
+  query {
+    metadata {
+      siteName
+    }
+  }
+</static-query>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  line-height: 1.5;
+}
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,31 @@
+<template>
+  <Layout>
+    <h1>Cameron Baney</h1>
+    <h2>Front-end Engineer</h2>
+    <p>
+      I am a front-end engineer focusing on JavaScript, Vue, Sass and semantic
+      HTML. My experience in design and UX helps create quality user friendly
+      websites and software.
+    </p>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {}
+}
+</script>
+
+<style lang="scss" scoped>
+h1 {
+  font-size: 4.5em;
+  font-weight: 500;
+}
+
+h2 {
+  font-size: 2.5em;
+}
+p {
+  font-size: 1.75em;
+}
+</style>
